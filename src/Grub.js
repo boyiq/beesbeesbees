391d40d36d1c5bde8e7553f1f89a729d9447f5af
Boyi Qu <boyiqu9596@gmail.com>
2022-09-12 11:45:34 -0700

    completed beesbeesbees

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,46 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
+
+Grub.prototype.eat = function() {
+
+};
+
+var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+};
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+let bee = new Bee();
+console.log(bee.age);
+
+
+var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+};
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
+
+var ForagerBee = function () {
+
+}
+
+
+
 
